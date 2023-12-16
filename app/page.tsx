6f23d0999c058d6ff624f8b7d92efcddc874bdc7
amevide998 <hdsvidi@gmail.com>
2023-12-16 20:35:24 +0700

    update content

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,47 @@ import {Project} from "@/types/Project";
 
 export default async function Home() {
 
-  const project = await getProjects()
+    const project = await getProjects()
 
-  return (
-      <div>
-          {
-              project.map((project) => (
-                  <div key={project._id}>
-                      <h1>{project.name}</h1>
-                  </div>
-              ))
-          }
-        
+    return (
+      <div className="max-w-5xl mx-auto py-20">
+          <h1 className="text-7xl font-extrabold">
+              Hello I&apos;m
+              <span className="bg-gradient-to-tr from-blue-500 via-purple-500 to-red-500 bg-clip-text text-transparent">
+                  {" "}vide !
+              </span>
+          </h1>
+
+          <p>
+              Hello everyone! Checkout my website
+          </p>
+
+          <h2 className="mt-24 font-bold text-gray-700 text-3xl">
+              My Project
+          </h2>
+
+          <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+              {
+                  project.map((project) => (
+                      <div key={project._id} className="border border-gray-500 rounded-lg p-3">
+                          <div className="font-extrabold bg-gradient-to-br from-green-400 to-blue-600 bg-clip-text text-transparent text-3xl">
+                              {project.name}
+                          </div>
+                          {
+                              project.image && (
+                                  <Image
+                                      src={project.image}
+                                      alt={project.name}
+                                      height={200}
+                                      width={750}
+                                      className="object-cover h-[200px] rounded-lg border border-gray-500"
+                                  />
+                              )
+                          }
+                      </div>
+                  ))
+              }
+          </div>
       </div>
-  )
+    )
 }
