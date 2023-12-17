9d9313b07abb5bb3ebce0d5de23be95e2ced0786
amevide998 <hdsvidi@gmail.com>
2023-12-17 15:30:57 +0700

    initial client config

---
+++ sanity/config/client-config.ts
@@ -0,0 +1,11 @@
+const config: {
+    projectId: string,
+    dataset: string,
+    apiVersion: string
+} = {
+    projectId: 'cnm4qcnn',
+    dataset: 'production',
+    apiVersion: '2023-12-16',
+}
+
+export default config
